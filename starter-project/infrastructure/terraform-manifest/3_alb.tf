aad420bf4be5beb38916d2723aa2e77a928e7f16
DINNYUY L FRU ANGU <100095136+Dinnyuy@users.noreply.github.com>
2024-10-08 00:14:59 +0100

    Create 3_alb.tf

---
+++ starter-project/infrastructure/terraform-manifest/3_alb.tf
@@ -0,0 +1,11 @@
+# 3_alb.tf
+module "alb" {
+  source = "./modules/alb"
+
+  cluster_name = local.cluster_name
+  environment  = var.environment
+  oidc_arn     = module.eks-cluster.oidc_provider_arn
+  oidc_url     = module.eks-cluster.cluster_oidc_issuer_url
+
+  depends_on = [module.eks-cluster]
+}
