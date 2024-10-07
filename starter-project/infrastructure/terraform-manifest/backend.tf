f953bfc1b088f173b60a636a28d6e1cbdadce85d
DINNYUY L FRU ANGU <100095136+Dinnyuy@users.noreply.github.com>
2024-10-07 21:58:13 +0100

    Update backend.tf

--- starter-project/infrastructure/terraform-manifest/backend.tf
+++ starter-project/infrastructure/terraform-manifest/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-bucket-77"
+    bucket         = "lemnyuyfru"
     key            = "terraform/terraform.state"
     region         = "us-east-1"
     dynamodb_table = "eks-terraform-state-77"
